58c07a9d145093712cd6862ae5a3962f9febd535
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-30 22:44:50 +0530

    installed dependencies

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const app = express();
