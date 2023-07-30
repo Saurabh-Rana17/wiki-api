10fe9a6f180771c18ee8812f732f448118bd1ef4
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-30 23:16:53 +0530

    server connected

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wikiDB");
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  const articleSchema = new mongoose.Schema({
+    title: String,
+    data: String,
+  });
+  let article = new mongoose.model("article", articleSchema);
+  app.get("/articles", async function (req, res) {
+    let newData = await article.find();
+    res.send(newData);
+  });
 }
 
 const app = express();
